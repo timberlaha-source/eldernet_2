edef8dae121b051f06bdd9ce318bd0851720f490
timberlaha-source <timberlaha@gmail.com>
2026-03-10 18:37:29 +0530

    changing of @ alias of imports in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
